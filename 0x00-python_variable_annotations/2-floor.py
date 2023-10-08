08c1495f55e1b0af38f8eec317cc8431d11e9fcb
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-07 19:18:37 -0700

    done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Args:
+        A float n.
+    Returns:
+        The floor of the float.
+    """
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        A float n.
+    Returns:
+        The string representation of the float.
+    """
+    return str(n)
