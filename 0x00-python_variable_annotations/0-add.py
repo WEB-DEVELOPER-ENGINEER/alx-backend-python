4bffa9edc00f6a0ba6b6f82f9ad99e957fcec7fe
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-07 19:01:23 -0700

    done

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Basic annotations - add"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Args:
+        a float a and a float b.
+    Returns:
+        returns their sum as a float.
+    """
+    return a + b
