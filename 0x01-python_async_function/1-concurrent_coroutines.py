5dce8721ab57dc52e51c78a74a8f94d3aab6cda5
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-19 06:47:22 -0700

    done

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""This script provides an async routine, wait_n, that generates random delays
+using the wait_random function. It spawns wait_random n times with a specified
+max_delay and returns a list of all the delays in ascending order, without
+using the sort() method due to concurrency considerations."""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Generate a list of ascending random delays asynchronously.
+
+    Parameters:
+    -----------
+    n (int): Number of delays to generate.
+    max_delay (int): Maximum delay in seconds.
+
+    Returns:
+    --------
+    List[float]: A list of ascending random delays.
+    """
+    delays = []
+    tasks = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+    return sorted(delays)
