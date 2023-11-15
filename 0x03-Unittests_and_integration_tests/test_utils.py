117e6c704d9d2a1e0468e0defcbc9ca1d64368dd
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-11-15 01:43:26 +0000

    done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -38,7 +38,8 @@ class TestGetJson(unittest.TestCase):
     ])
     def test_get_json(self, test_url, test_payload):
         """test that utils.get_json returns the expected result"""
-        patcher = patch('requests.get')
+        config = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **config)
         mock = patcher.start()
         self.assertEqual(get_json(test_url), test_payload)
         mock.assert_called_once()
